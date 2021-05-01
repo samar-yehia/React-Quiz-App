6bb5c431c5c0d794436d1a0f9bce74c8abb4f832
Samar Yehia <samaryehiaabdelaziz@gmail.com>
2021-05-01 21:32:43 +0200

    - Created Landing Page - Added CSS Classes

---
+++ client/src/components/LandingPage.jsx
@@ -0,0 +1,10 @@
+function LandingPage() {
+  return (
+    <div className="LandingPage-WelcomeHeader">
+      <h1>Welcome To The Quiz</h1>
+      <button className="CTA-LandingPage-button"> START NOW</button>
+    </div>
+  );
+}
+
+export default LandingPage;
