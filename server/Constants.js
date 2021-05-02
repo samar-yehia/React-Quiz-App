7f6295e056d6c383630047c9e06ecfac4fc37192
Omar YAYA <47383263+omaryaya@users.noreply.github.com>
2021-05-03 00:20:25 +0200

    Added configurations for local development.

--- server/Constants.js
+++ server/Constants.js
@@ -1,2 +1,6 @@
-export const CONNECTION_URL = "mongodb://mongo:27017/?retryWrites=true";
+// Uncomment the following line for docker deployment
+// export const CONNECTION_URL = "mongodb://mongo:27017/?retryWrites=true";
+
+// uncomment the following line for local dev
+export const CONNECTION_URL = "mongodb://localhost:27888/?retryWrites=true";
 export const PORT = process.env.PORT || 5000;
